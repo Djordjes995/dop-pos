534c0a4b10c0d2a729d9b38330091a051b5a1356
GroovyGeorge <djolesfon@gmail.com>
2023-08-12 18:12:07 +0200

    container for dashboard

--- src/layouts/dashboard/Dashboard.tsx
+++ src/layouts/dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { styled } from '@mui/material/styles';
 import { SideNav } from './SideNav';
+import Container from '@mui/material/Container';
 
 const SIDE_NAV_WIDTH = 280;
 
@@ -17,13 +18,14 @@ const LayoutContainer = styled('div')({
   display: 'flex',
   flex: '1 1 auto',
   flexDirection: 'column',
-  width: '100%'
+  width: '100%',
+  minHeight: '100vh',
 });
 
 export const Dashboard = ((props: any) => {
   const { children } = props;
   const [openNav, setOpenNav] = useState(false);
-
+  
   return (
     <>
       <SideNav
@@ -31,8 +33,10 @@ export const Dashboard = ((props: any) => {
         open={openNav}
       />
       <LayoutRoot>
-        <LayoutContainer sx={{ fontFamily: (theme) => theme.typography.fontFamily }}>
-          {children}
+        <LayoutContainer sx={{ fontFamily: (theme) => theme.typography.fontFamily, backgroundColor: (theme) => theme.palette.grey[50] }}>
+          <Container maxWidth="xl" sx={{ marginTop: 4 }}>
+            {children}
+          </Container>
         </LayoutContainer>
       </LayoutRoot>
     </>
