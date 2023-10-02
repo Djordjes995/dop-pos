87d5cba90561e6d3b72a10aea3bf404eb6270949
GroovyGeorge <djolesfon@gmail.com>
2023-10-02 11:11:58 +0200

    regular order total logic

--- src/components/RegularOrderTable.tsx
+++ src/components/RegularOrderTable.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import {
   ColumnDirective,
   ColumnsDirective,
@@ -5,6 +6,7 @@ import {
   Inject,
   Resize
 } from "@syncfusion/ej2-react-grids";
+import { useQueryClient  } from '@tanstack/react-query'
 import { by_sku } from '../assets/mock';
 import { TextField, Box } from '@mui/material';
 import { useTranslation } from 'react-i18next';
@@ -17,8 +19,25 @@ const RegularOrderTable = () => {
 
   const { isLoading, error, data } = useQuery({
     queryKey: ['regularOrder'],
-    queryFn: regularOrderAction,
+    queryFn: fillQuantities,
   })
+
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (!data) return
+    // Create a function to compare two objects by a specific field
+    const hasFieldChanged = (prev, current, field) => prev[field] !== current[field];
+
+    // Check if the 'customQuantity' field of any object has changed
+    const hasNameChanged = data?.suggested_order_items?.some((item, index) =>
+      hasFieldChanged(item, data.suggested_order_items[index], 'customQuantity')
+    );
+
+    if (hasNameChanged) {
+      console.log('Name field has changed in one of the objects.');
+    }
+  }, [data]);
 
   if (isLoading) return 'Loading...'
 
@@ -26,15 +45,42 @@ const RegularOrderTable = () => {
 
   let grid: any;
 
-  const quantityTemplate = ({ quantity } = props) => {
+  async function fillQuantities() {
+    try {
+      const result = await regularOrderAction()
+      result.suggested_order_items = result.suggested_order_items.map(item => {
+        return { ...item, customQuantity: item.quantity || 0 }
+      })
+      return result
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  // Update the data on input change
+  const handleInputChange = (e, id) => {
+    const newValue = e.target.value;
+
+    // Update the data in React Query cache
+    // You might need to replace 'getData' with the actual query key used in your app
+    // This updates the 'value' field of the specific item in the cache
+    // without triggering a re-fetch
+    queryClient.setQueryData('regularOrder', (prevData) =>
+      prevData.suggested_order_items.map((item) =>
+        item.item_id === item_id ? { ...item, value: newValue } : item
+      )
+    );
+  };
+
+  const quantityTemplate = ({ customQuantity, item_id } = props) => {
     return <Box sx={{ height: 24, width: 40 }}>
-      <TextField defaultValue={quantity} variant="standard" type="number" size="small" />
+      <TextField defaultValue={customQuantity} onChange={(e) => handleInputChange(e, item_id)} variant="standard" type="number" size="small" />
     </Box>
   };
 
   const totalTemplate = (props) => {
     return <Box sx={{ height: 24 }}>
-      {parseInt(props.size) * parseInt(props.quantity)}
+      {parseInt(props.size) * parseInt(props.customQuantity)}
     </Box>
   };
 
@@ -58,7 +104,7 @@ const RegularOrderTable = () => {
         <ColumnDirective field="suggested_quantity" headerText={t('regularOrder.suggestedQuantity')} />
         <ColumnDirective field="order_history_quantity" headerText={t('regularOrder.history')} />
         <ColumnDirective field="quantity" headerText={t('regularOrder.quantity')} template={quantityTemplate} />
-        <ColumnDirective field="total" headerText={t('regularOrder.total')} template={totalTemplate}/>
+        <ColumnDirective field="total" headerText={t('regularOrder.total')} template={totalTemplate} />
       </ColumnsDirective>
     </GridComponent>
   );
