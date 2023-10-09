b6dc3dcbe12ae4e516735fd9be9f997ec2546b71
GroovyGeorge <djolesfon@gmail.com>
2023-10-09 22:59:38 +0200

    theme header

--- src/layouts/dashboard/Header.tsx
+++ src/layouts/dashboard/Header.tsx
@@ -1,63 +1,125 @@
-import React, { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import { deepOrange } from '@mui/material/colors';
+import PropTypes from 'prop-types';
+import BellIcon from '@heroicons/react/24/solid/BellIcon';
+import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
+import Bars3Icon from '@heroicons/react/24/solid/Bars3Icon';
+import MagnifyingGlassIcon from '@heroicons/react/24/solid/MagnifyingGlassIcon';
+import {
+  Avatar,
+  Badge,
+  Box,
+  IconButton,
+  Stack,
+  SvgIcon,
+  Tooltip,
+  useMediaQuery
+} from '@mui/material';
+import { alpha } from '@mui/material/styles';
+import { usePopover } from '../../hooks/usePopover';
+import { AccountPopover } from './AccountPopover';
 
-export const Header = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
+const SIDE_NAV_WIDTH = 280;
+const TOP_NAV_HEIGHT = 64;
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+export const Header = (props) => {
+  const { onNavOpen } = props;
+  const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'));
+  const accountPopover = usePopover();
 
   return (
-    // <--! transparent bg -->
-    <AppBar position="static" style={{ height: '80px', border: 'none', boxShadow: 'none', backgroundColor: 'blue' }}>
-      <Toolbar sx={{ justifyContent: 'space-between' }}>
-        <Typography variant="h6" component="div">
-        </Typography>
-        <div>
-          <IconButton
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleClick}
-            color="inherit"
+    <>
+      <Box
+        component="header"
+        sx={{
+          backdropFilter: 'blur(6px)',
+          backgroundColor: (theme) => alpha(theme.palette.background.default, 0.8),
+          position: 'sticky',
+          left: {
+            lg: `${SIDE_NAV_WIDTH}px`
+          },
+          top: 0,
+          width: {
+            lg: '100%'
+          },
+          zIndex: (theme) => theme.zIndex.appBar
+        }}
+      >
+        <Stack
+          alignItems="center"
+          direction="row"
+          justifyContent="space-between"
+          spacing={2}
+          sx={{
+            minHeight: TOP_NAV_HEIGHT,
+            px: 2
+          }}
+        >
+          <Stack
+            alignItems="center"
+            direction="row"
+            spacing={2}
           >
-            <Avatar sx={{ bgcolor: 'green' }}>
-              amv
-            </Avatar>
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
+            {!lgUp && (
+              <IconButton onClick={onNavOpen}>
+                <SvgIcon fontSize="small">
+                  <Bars3Icon />
+                </SvgIcon>
+              </IconButton>
+            )}
+            <Tooltip title="Search">
+              <IconButton>
+                <SvgIcon fontSize="small">
+                  <MagnifyingGlassIcon />
+                </SvgIcon>
+              </IconButton>
+            </Tooltip>
+          </Stack>
+          <Stack
+            alignItems="center"
+            direction="row"
+            spacing={2}
           >
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleClose}>My Account</MenuItem>
-            <MenuItem onClick={handleClose}>Logout</MenuItem>
-          </Menu>
-        </div>
-      </Toolbar>
-    </AppBar>
+            <Tooltip title="Contacts">
+              <IconButton>
+                <SvgIcon fontSize="small">
+                  <UsersIcon />
+                </SvgIcon>
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Notifications">
+              <IconButton>
+                <Badge
+                  badgeContent={4}
+                  color="success"
+                  variant="dot"
+                >
+                  <SvgIcon fontSize="small">
+                    <BellIcon />
+                  </SvgIcon>
+                </Badge>
+              </IconButton>
+            </Tooltip>
+            <Avatar
+              onClick={accountPopover.handleOpen}
+              ref={accountPopover.anchorRef}
+              sx={{
+                cursor: 'pointer',
+                height: 40,
+                width: 40
+              }}
+              src="/assets/avatars/avatar-anika-visser.png"
+            />
+          </Stack>
+        </Stack>
+      </Box>
+      <AccountPopover
+        anchorEl={accountPopover.anchorRef.current}
+        open={accountPopover.open}
+        onClose={accountPopover.handleClose}
+      />
+    </>
   );
+};
+
+Header.propTypes = {
+  onNavOpen: PropTypes.func
 };
